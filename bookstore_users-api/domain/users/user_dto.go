0c64aff300e18c0646dab31ffac8bab69378810b
mrmtsu <81202muramatsu@gmail.com>
2021-06-17 07:35:02 +0900

    add: find_rows

--- bookstore_users-api/domain/users/user_dto.go
+++ bookstore_users-api/domain/users/user_dto.go
@@ -6,11 +6,17 @@ import (
 	"github.com/mrmtsu/go-book/bookstore_users-api/utils/errors"
 )
 
+const (
+	StatusActive = "active"
+)
+
 type User struct {
 	Id          int64  `json:"id"`
 	FirstName   string `json:"first_name"`
 	LastName    string `json:"last_name"`
 	Email       string `json:"email"`
+	Status      string `json:"status"`
+	Password    string `json:"password"`
 	DateCreated string `json:"date_created"`
 }
 
@@ -21,6 +27,11 @@ func (u *User) Validate() *errors.RestErr {
 	u.Email = strings.TrimSpace(strings.ToLower(u.Email))
 	if u.Email == "" {
 		return errors.NewBadRequestError("invalid email address")
+	}
+
+	u.Password = strings.TrimSpace(u.Password)
+	if u.Password == "" {
+		return errors.NewBadRequestError("invalid email password")
 	}
 	return nil
 }
--- bookstore_users-api/services/users_service.go
+++ bookstore_users-api/services/users_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"github.com/mrmtsu/go-book/bookstore_users-api/domain/users"
+	"github.com/mrmtsu/go-book/bookstore_users-api/utils/date_utils"
 	"github.com/mrmtsu/go-book/bookstore_users-api/utils/errors"
 )
 
@@ -18,6 +19,8 @@ func CreateUser(u users.User) (*users.User, *errors.RestErr) {
 		return nil, err
 	}
 
+	u.Status = users.StatusActive
+	u.DateCreated = date_utils.GetNowDBFormat()
 	if err := u.Save(); err != nil {
 		return nil, err
 	}
@@ -58,4 +61,9 @@ func UpdateUser(isPartial bool, u users.User) (*users.User, *errors.RestErr) {
 func DeleteUser(userId int64) *errors.RestErr {
 	user := &users.User{Id: userId}
 	return user.Delete()
+}
+
+func Search(status string) ([]users.User, *errors.RestErr) {
+	dao := &users.User{}
+	return dao.FindByStatus(status)
 }
